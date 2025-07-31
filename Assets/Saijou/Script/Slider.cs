5cd6773561659aeef2c36e4925ee651d8baaea2c
saijou <>
2025-07-31 13:30:59 +0900

    プレス機（左右）、スライダー完了。ばね途中

---
+++ Assets/Saijou/Script/Slider.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Slider : MonoBehaviour
+{
+    public Transform player;
+    public float attackRangeX = 10f; //攻撃判定距離
+    public float speed = 5f;         //攻撃スピード
+
+    private Rigidbody rb;
+    private Collider col;
+    private bool isSlide = false;
+    private float slideDirection = 0f;//スライダーの方向
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        float xDistance = Mathf.Abs(transform.position.x - player.position.x);
+
+        if(!isSlide && xDistance <= attackRangeX)
+        {
+            isSlide = true;
+
+            //最初の方向を記録
+            slideDirection = Mathf.Sign(player.position.x - transform.position.x);
+
+            //もしPlayerと同じ位置になったら（0）
+            if (slideDirection == 0)
+            {
+
+            }
+        }
+        if (xDistance > attackRangeX)
+        {
+            isSlide = false;
+            rb.velocity = Vector2.zero;//止まる
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isSlide)
+        {
+           transform.position += new Vector3(slideDirection, 0, 0) * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            col.enabled = false;//playerと接触したら通過する
+        }
+    }
+}
